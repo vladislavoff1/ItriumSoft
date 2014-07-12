c514c0b011e7be7500204d0fdc2f619e62ece203
Vlad <vlad007.1996@yandex.ru>
2014-07-12 19:33:39 +0400

    Include IP Connector Emulator to repository.

---
+++ IPConnectorEmulator/IPConnector.java
@@ -0,0 +1,17 @@
+/*
+How to use:
+
+java IPConnector [-options] command [args..]
+Where command include:
+	-s  
+
+
+*/
+
+class IPConnector {
+	public static void main(String[] args) {
+		
+
+
+	}
+}
