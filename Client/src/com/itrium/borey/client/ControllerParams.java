814dffce951df6bb522b138d6528e5c556a2b123
Vlad <vlad007.1996@yandex.ru>
2014-07-23 03:07:16 +0400

    Helps to send controller's parameters to some function.

---
+++ Client/src/com/itrium/borey/client/ControllerParams.java
@@ -0,0 +1,13 @@
+package com.itrium.borey.client;
+
+/**
+ * @author Vladislav Romanov
+ */
+
+public class ControllerParams {
+
+    public String key;
+    public String time;
+    public String status;
+
+}
